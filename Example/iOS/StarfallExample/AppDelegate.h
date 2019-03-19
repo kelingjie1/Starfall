f5773889a2ef8f5256458b67f84427a904dfe2e3
lingtonke <lingtonke@tencent.com>
2019-03-19 17:20:15 +0800

    project

---
+++ Example/iOS/StarfallExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarfallExample
+//
+//  Created by lingtonke on 2019/3/19.
+//  Copyright © 2019 Starfall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/iOS/StarfallExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StarfallExample
+//
+//  Created by lingtonke on 2019/3/19.
+//  Copyright © 2019 Starfall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
