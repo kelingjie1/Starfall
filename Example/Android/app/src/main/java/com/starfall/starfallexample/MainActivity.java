a0b14e503efe9e99f00a8a50646b97b9995f37fc
willisdai <willisdai@willisdai-mc0.tencent.com>
2019-07-05 15:33:55 +0800

    update

--- Example/Android/app/src/main/java/com/starfall/starfallexample/MainActivity.java
+++ Example/Android/app/src/main/java/com/starfall/starfallexample/MainActivity.java
@@ -11,9 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
     }
 
     /**
